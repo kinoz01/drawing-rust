268f3ae80e47f52ff0ebcf25617143c3de08e902
aammar <ayoub-ammar@protonmail.com>
2025-07-30 18:20:42 +0100

    (\|) ._. (|/)

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 mod geometrical_shapes;
 
 use geometrical_shapes as gs;
-use gs::{Displayable, Drawable};
-use raster::{Color, Image};
+use gs::{ Displayable, Drawable };
+use raster::{ Color, Image };
 
 fn main() {
     let mut image = Image::blank(1000, 1000);
@@ -14,10 +14,10 @@ fn main() {
     let rectangle = gs::Rectangle::new(&gs::Point::new(150, 150), &gs::Point::new(50, 50));
     rectangle.draw(&mut image);
 
-    let triangle = gs::Triangle::new (
-            &gs::Point::new(500, 500),
-            &gs::Point::new(250, 700),
-            &gs::Point::new(700, 800),
+    let triangle = gs::Triangle::new(
+        &gs::Point::new(500, 500),
+        &gs::Point::new(250, 700),
+        &gs::Point::new(700, 800)
     );
     triangle.draw(&mut image);
 
